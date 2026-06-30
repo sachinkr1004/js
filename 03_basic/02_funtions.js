f978d3c28dd7586ebfc656a3705b65ca36dd4980
sachinkr1004 <143534422+sachinkr1004@users.noreply.github.com>
2026-06-30 17:24:21 +0000

    02functionsAdd

--- 03_basic/02_funtions.js
+++ 03_basic/02_funtions.js
@@ -12,3 +12,48 @@ if (true) {
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one(){
+    const username = "sachin"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "sachin"
+    if (username === "sachin") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
